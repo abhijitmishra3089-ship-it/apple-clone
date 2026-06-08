91498a8cfb494aaad8e5b6f753dc00cb2048b85f
abhijit mishra <abhijitmishra3089@gmail.com>
2026-06-08 10:34:25 +0530

    third

---
+++ cookie.js
@@ -0,0 +1,11 @@
+// function getvalue(key) {
+//     const cookies=document.cookie.split('; ');
+//     for(let i=0;i<cookeis.length;i++){
+//         const[key,value]=cookies[i].split('=')
+//         if(key===key){
+//             return value
+//         }
+//     }
+// }
+
+// console.log()
